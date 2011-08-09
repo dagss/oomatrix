7dd4d0de4e909763be26ab5a48dcfd67d33fe8f4
Eirik Gjerloew <egjerlow@gmail.com>
2011-08-09 12:03:02 +0200

    Added multiplication and addition with numpy arrays

--- oomatrix/dense_matrix.py
+++ oomatrix/dense_matrix.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 class Matrix(object):
     def __add__(self, other):
@@ -9,6 +10,8 @@ class Matrix(object):
     def __mul__(self, other):
         if isinstance(other, Matrix):
             return MultipliedMatrices([self, other])
+        elif isinstance(other, np.ndarray):
+            return self.apply(other)
         else:
             raise NotImplementedError()
 
@@ -17,6 +20,14 @@ class Matrix(object):
         if expr[0] == '(':
             expr = expr[1:-1]
         return "%dx%d matrix: %s" % (self.shape[0], self.shape[1], expr)
+
+    def apply(self, other):
+        #TODO: Check for compatibility
+        return self._apply(other)
+    
+    def _apply(self, other):
+        raise NotImplementedError()
+        
 
 class AddedMatrices(Matrix):
     def __init__(self, matrices):
@@ -32,11 +43,16 @@ class AddedMatrices(Matrix):
         elif isinstance(other, Matrix):
             return AddedMatrices(self.matrices + [other])
         else:
-            return Matrix.__add__(other)
+            return Matrix.__add__(self, other)
 
     def expression_string(self):
         return "(%s)" % " + ".join(x.expression_string() for x in self.matrices)
 
+    def _apply(self, vec):
+        sum = 0
+        for M in self.matrices:
+            sum += M._apply(vec)
+        return sum
 
 class MultipliedMatrices(Matrix):
     def __init__(self, matrices):
@@ -52,12 +68,15 @@ class MultipliedMatrices(Matrix):
         elif isinstance(other, Matrix):
             return MultipliedMatrices(self.matrices + [other])
         else:
-            return Matrix.__add__(other)
+            return Matrix.__mul__(self, other)
 
     def expression_string(self):
         return "(%s)" % " * ".join(x.expression_string() for x in self.matrices)
 
-
+    def _apply(self, vec):
+        for M in self.matrices[::-1]:
+            vec = M._apply(vec)
+        return vec
 
 class DenseMatrix(Matrix):
     def __init__(self, array, name):
@@ -68,3 +87,8 @@ class DenseMatrix(Matrix):
     def expression_string(self):
         return "%s" % self.name
 
+    def _apply(self, other):
+        #TODO: Check for compatibility
+        if other.ndim > 2:
+            raise NotImplementedError()
+        return np.dot(self.array, other)
--- oomatrix/test/test_dense_matrix.py
+++ oomatrix/test/test_dense_matrix.py
@@ -1,4 +1,5 @@
 from nose.tools import ok_, eq_, assert_raises
+from numpy.testing import assert_almost_equal
 import numpy as np
 
 from oomatrix import DenseMatrix
@@ -26,5 +27,13 @@ def test_repr():
     yield eq_, '10x10 matrix: (A * A) + (A * B)', repr(A * A + A * B)
     yield eq_, '10x10 matrix: ((A * A) + A) * B', repr((A * A + A) * B)
 
-    
-    
+def test_matvec():
+    A = DenseMatrix(array, "A")
+    B = DenseMatrix(array, "B")
+    u = ndrange((10, 1))
+    v = ndrange((10, 2, 3, 4))
+
+    yield ok_, np.all(np.dot(array, u) == A * u)
+    yield ok_, np.all(np.dot(array, np.dot(array, u)) == A * A * u)
+    yield ok_, np.all(np.dot((array + array), u) == (A + A) * u)
+    #yield eq_, (5, 2, 3, 4), (DenseMatrix(ndrange((5, 10)), 'tmp') * v).shape
