c8b837060567c737bde27c9b5262c60d5a2688f8
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2011-09-29 12:10:13 +0200

    test_graphs: More unit-test-like unit test

--- oomatrix/test/test_graphs.py
+++ oomatrix/test/test_graphs.py
@@ -1,34 +1,44 @@
 import numpy as np
+from nose import SkipTest
 from nose.tools import ok_, eq_, assert_raises
 
-from ..core import addition_conversion_graph, MatrixImplType
-from ..shortest_path import find_shortest_path
+from ..core import addition_conversion_graph, MatrixImpl, add_operation, conversion
 from ..impl.diagonal import *
 from ..impl.dense import *
 
 from .. import Matrix
 
+mock_kinds = []
 
+for X in 'ABCD':
+    class MockKind(MatrixImpl):
+        name = X
+    mock_kinds.append(MockKind)
 
+    @add_operation((MockKind, MockKind), MockKind)
+    def f(a, b):
+        return 
 
+A, B, C, D = mock_kinds
 
-De = Matrix('De', np.arange(9).reshape(3, 3).astype(np.int64))
-Di = Matrix('Di', np.arange(3).astype(np.int64), diagonal=True)
-
+# TODO: Mock full AdditionGraph
 
 def test_get_vertices():
-    C, R, S, D = ColumnMajorImpl, RowMajorImpl, SymmetricContiguousImpl, DiagonalImpl
-    V = list(addition_conversion_graph.get_vertices(3, [C, R, S, D]))
-    V0 = [[C], [R], [S], [D],
-          [C, R], [C, S], [C, D], [R, S], [R, D], [S, D],
-          [C, R, S], [C, R, D], [C, S, D], [R, S, D]]
+    V = list(addition_conversion_graph.get_vertices(3, [A, B, C, D]))
+    V0 = [[A], [B], [C], [D],
+          [A, B], [A, C], [A, D], [B, C], [B, D], [C, D],
+          [A, B, C], [A, B, D], [A, C, D], [B, C, D]]
     V0 = [set(x) for x in V0]
     assert len(V0) == len(V)
     for v in V:
         assert v in V0
 
 def test_add_perform():
-    r = addition_conversion_graph.perform([Di.get_impl(), Di.get_impl()])
+    raise SkipTest()
+    a = A()
+    b = B()
+    
+    r = addition_conversion_graph.perform([A, A])
     print Matrix('R', r)
 
     r = addition_conversion_graph.perform([Di.get_impl(), De.get_impl()])
