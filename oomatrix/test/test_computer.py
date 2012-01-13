f68fe027dc3ab0eb947dfce20dbce1a2e76c4674
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2012-01-13 11:48:06 +0100

    TST StupidComputer conjugate transpose

--- oomatrix/test/test_computer.py
+++ oomatrix/test/test_computer.py
@@ -24,34 +24,35 @@ class Mock1(MatrixImpl):
     def __repr__(self):
         return '<%s:%s>' % (type(self).name, self.value)
 
-def _(self):
+def conjugate_repr(self):
     return '<%s:%s.h>' % (type(self).name, self.wrapped.value)
     
-Mock1.h.__repr__ = _
+Mock1.h.__repr__ = conjugate_repr
 
 class Mock2(Mock1):
     name = 'Mock2'
+Mock2.h.__repr__ = conjugate_repr
 
-# Can only multiply (1 1) and (1 2)
 @mock_multiply_operation((Mock1, Mock1), Mock1)
 def mul(a, b):
     assert a.ncols == b.nrows
     return Mock1('(%s %s)' % (a.value, b.value), a.nrows, b.ncols)
+
+@mock_multiply_operation((Mock1, Mock1.h), Mock1)
+def mul(a, b):
+    assert a.ncols == b.nrows
+    return Mock1('(%s %s.h)' % (a.value, b.wrapped.value), a.nrows, b.ncols)
 
 @mock_multiply_operation((Mock1, Mock2), Mock2)
 def mul(a, b):
     assert a.ncols == b.nrows
     return Mock2('(%s %s)' % (a.value, b.value), a.nrows, b.ncols)
 
+
 @mock_multiply_operation((Mock1.h, Mock2), Mock2)
 def mul(a, b):
     assert a.ncols == b.nrows
-    return Mock2('(%s.h %s)' % (a.value, b.value), a.nrows, b.ncols)
-
-@mock_multiply_operation((Mock1, Mock2.h), Mock2)
-def mul(a, b):
-    assert a.ncols == b.nrows
-    return Mock2('(%s %s.h)' % (a.value, b.value), a.nrows, b.ncols)
+    return Mock2('(%s %s.h)' % (a.wrapped.value, b.value), a.nrows, b.ncols)
 
 A = Matrix(Mock1('A', 3, 3))
 B = Matrix(Mock2('B', 3, 4))
@@ -68,25 +69,27 @@ v = Matrix(Mock2('v', 4, 1))
 def test_stupid_computer_mock():
     computer = StupidComputer(multiply_graph=mock_multiply_graph)
     co = computer.compute
-    def do(M):
-        return repr(co(M)._expr.matrix_impl)
+    def test(expected, M):
+        eq_(expected, repr(co(M)._expr.matrix_impl))
 
     # A is 3-by-3 Mock1, B is 3-by-4 Mock2, u is 3-by-1 Mock2
     yield ok_, type(A * u) is Matrix
-    yield eq_, '<Mock2:(A u)>', do(A * u)
-    yield eq_, '<Mock2:(A (A u))>', do(A * A * u)
-    yield eq_, '<Mock2:(A (A (A u)))>', do(A * A * A * u)
-    yield eq_, '<Mock2:(((A A) A) B)>', do(A * A * A * B)
+    yield test, '<Mock2:(A u)>', A * u
+    yield test, '<Mock2:(A (A u))>', A * A * u
+    yield test, '<Mock2:(A (A (A u)))>', A * A * A * u
+    yield test, '<Mock2:(((A A) A) B)>', A * A * A * B
 
-    yield eq_, '<conjugate transpose Mock1:(A A).h>', do(A.h * A.h)
 
     # Straightforward conjugate
-    yield eq_, '<Mock1:(Ah A)>', do(A.h * A)
-    # A * A.h is not implemented directly, so requires reformulation
-    yield eq_, '<Mock1:(A Ah)>', do(A * A.h)
-    yield eq_, '<Mock2:(Ah (A( Ahu)))>', do(A.h * A * A.h * u)
-    yield assert_raises, ImpossibleOperationError, do, u.h * A.h
-    yield eq_, '<Mock1:(xhAh)>', do(Matrix(Mock1('x', 3, 1)).h * A.h)
+    yield test, '<Mock1:(A A.h)>', A * A.h
+    # Need to conjugate parent expression
+    yield test, '<conjugate transpose Mock1:(A A).h>', A.h * A.h
+    # A.h * A is not implemented
+    yield assert_raises, ImpossibleOperationError, co, A.h * A
+    # But A.h * B is (need to conjugate parent)
+    yield test, '<conjugate transpose Mock2:(A B.h).h>', B.h * A
+
+    yield test, '<conjugate transpose Mock1:(A x).h>', Matrix(Mock1('x', 3, 1)).h * A.h
 
 
 def test_stupid_computer_numpy():
