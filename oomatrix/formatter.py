f2af5b880aaff37c4829f676806b889a645a98d9
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2012-04-13 11:55:43 +0200

    BUG explain()

--- oomatrix/formatter.py
+++ oomatrix/formatter.py
@@ -79,7 +79,7 @@ class Explainer(object):
             self.computable_names[expr] = name
         self.stream.write('Computing expression:\n\n    %s\n\n' % expr_str)
         self.stream.write('by:\n\n')
-        self.process(self.computable_root, 'Root')
+        self.process(self.computable_root)
 
     def format_expression(self, expr):
         return self.expression_formatter.format(expr)
@@ -103,28 +103,32 @@ class Explainer(object):
     def dedent(self):
         self.num_indents -= 1
 
-    def process(self, node, prefix):
-        node.accept_visitor(self, node, prefix)
+    def process(self, node):
+        node.accept_visitor(self, node)
 
-    def visit_computable(self, node, prefix):
+    def describe_operation(self, result, computation_name, args,
+                           result_kind_name, cost):
+        self.putln('{0} = {1}({2}) [{3} result, cost={4}]',
+                   result, computation_name, ', '.join(args),
+                   result_kind_name, cost)
+
+    def process_operation(self, node, computation_name):
         labels = []
         for child in node.children:
-            labels.append(child.accept_visitor(self, child, ''))
+            labels.append(child.accept_visitor(self, child))
         label = self.get_label(node)
-        self.putln('{0} = {1}({2}) [{3} result, cost={4}]',
-                   label,
-                   node.computation.name,
-                   ', '.join(labels),
-                   node.kind.name,
-                   node.cost,
-                   )
+        self.describe_operation(label,
+                                computation_name,
+                                labels,
+                                node.kind.name,
+                                node.cost,
+                                )
         return label
-        
-        #self.dedent()    
-        #self.putln()
-        #print 'Computable', node, node.__dict__
 
-    def visit_leaf(self, node, prefix):
+    def visit_computable(self, node):
+        return self.process_operation(node, node.computation.name)
+
+    def visit_leaf(self, node):
         label = self.get_label(node)
         return label
         #name = node.name
@@ -132,8 +136,14 @@ class Explainer(object):
         #    name = 'a %s matrix' % node.matrix_impl.name
         #return name
 
-    def visit_conjugate_transpose(self, node, prefix):
-        node.child.accept_visitor(self, node.child, prefix)
+    def visit_conjugate_transpose(self, node):
+        node.child.accept_visitor(self, node.child)
+
+    def visit_decomposition(self, node):
+        print node, node.__dict__
+        return self.process_operation(node,
+                                      node.decomposition.get_name(node.kind))
+    
 
 class NoopWriter(object):
     def putln(self, line, *args, **kw):
