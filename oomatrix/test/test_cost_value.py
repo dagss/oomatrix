ff4429643a7b54456c1add84b5deb05de7483551
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2012-05-15 10:14:36 +0200

    TST

--- oomatrix/test/test_cost_value.py
+++ oomatrix/test/test_cost_value.py
@@ -16,14 +16,14 @@ def test_identities():
 def test_zero():
     yield eq_, '0', repr(FLOP - FLOP)
     yield eq_, '0', repr(0 * FLOP)
-    yield eq_, '3 MEMOP', repr(FLOP + 3 * MEMOP - FLOP)
+    yield eq_, '3.00e+00 MEMOP', repr(FLOP + 3 * MEMOP - FLOP)
     
 
 def test_distributive_law():
     yield eq_, 4 * FLOP + 6 * MEM, 2 * (2 * FLOP + 3 * MEM)
 
 def test_repr():
-    yield eq_, '4 FLOP + 2 MEM', repr(4 * FLOP + 2 * MEM)
+    yield eq_, '4.00e+00 FLOP + 2.00e+00 MEM', repr(4 * FLOP + 2 * MEM)
 
 def test_weigh():
     yield eq_, 5, CostValue(FLOP=1, MEM=2).weigh(dict(MEM=2, FLOP=1))
